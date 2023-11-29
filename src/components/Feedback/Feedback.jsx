4d4c0884b06de56121867a934ff7e48c2781c6be
Bartłomiej Gal <bgal23@Bartomiejs-MacBook-Pro.local>
2023-11-29 12:32:04 +0100

    fff

--- src/components/Feedback/Feedback.jsx
+++ src/components/Feedback/Feedback.jsx
@@ -10,14 +10,25 @@ export class FeedbackOptions extends Component {
   state = { ...state };
 
   handleClick = event => {
-    console.log(event.target);
+    const name = event.target.innerText.toLowerCase();
+    this.setState({
+      state: event.target.innerText.toLowerCase() === name && (state.good += 1),
+    });
+    console.log(state);
   };
+
   render() {
+    const { good, neutral, bad } = this.state;
     return (
       <div name={this.state} onClick={this.handleClick}>
         <button type="button">Good</button>
         <button type="button">Neutral</button>
         <button type="button">Bad</button>
+        <ul>
+          <li>Good: {good}</li>
+          <li>Neutral: {neutral}</li>
+          <li>Bad: {bad}</li>
+        </ul>
       </div>
     );
   }
