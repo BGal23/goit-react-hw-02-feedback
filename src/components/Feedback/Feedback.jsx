226610462b497235da4132194cb54852d8d6b7b3
Bartłomiej Gal <bgal23@Bartomiejs-MacBook-Pro.local>
2023-11-29 20:18:25 +0100

    duba

--- src/components/Feedback/Feedback.jsx
+++ src/components/Feedback/Feedback.jsx
@@ -1,34 +1,27 @@
 import { Component } from 'react';
-
-const state = {
-  good: 0,
-  neutral: 0,
-  bad: 0,
-};
+import state from './state.json';
 
 export class FeedbackOptions extends Component {
   state = { ...state };
 
   handleClick = event => {
-    const name = event.target.innerText.toLowerCase();
-    this.setState({
-      state: event.target.innerText.toLowerCase() === name && (state.good += 1),
-    });
+    if (event.target.type === 'button') {
+      const name = event.target.innerText.toLowerCase();
+      this.setState({
+        state:
+          event.target.innerText.toLowerCase() === name && (state[name] += 1),
+      });
+    }
+
     console.log(state);
   };
 
   render() {
-    const { good, neutral, bad } = this.state;
     return (
-      <div name={this.state} onClick={this.handleClick}>
+      <div onClick={this.handleClick}>
         <button type="button">Good</button>
         <button type="button">Neutral</button>
         <button type="button">Bad</button>
-        <ul>
-          <li>Good: {good}</li>
-          <li>Neutral: {neutral}</li>
-          <li>Bad: {bad}</li>
-        </ul>
       </div>
     );
   }
--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -1,7 +1,15 @@
 import { Component } from 'react';
+import state from '../Feedback/state.json';
 
 export class Statistics extends Component {
   render() {
-    return <p>Hello</p>;
+    const { good, neutral, bad } = state;
+    return (
+      <ul>
+        <li>Good: {good}</li>
+        <li>Neutral: {neutral}</li>
+        <li>Bad: {bad}</li>
+      </ul>
+    );
   }
 }
